16423380d0cf3f25d9081cb432ade78a09a9b4f7
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-06-23 21:40:42 -0300

    feat: criando componente para a lista de ingredientes selecionados

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,10 +1,11 @@
 <script lang="ts">
 import SelecionarIngredientes from "./SelecionarIngredientes.vue";
-import Tag from "./Tag.vue";
+import SuaLista from "./SuaLista.vue";
+
 export default {
   components: {
     SelecionarIngredientes,
-    Tag,
+    SuaLista,
   },
   data() {
     return {
@@ -25,24 +26,7 @@ export default {
 
 <template>
   <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
-
-      <ul v-if="ingredientes.length != 0" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente">
-          <Tag :texto="ingrediente" ativa />
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img
-          src="../assets/images/icones/lista-vazia.svg"
-          alt="Ícone de pesquisa"
-        />
-        Sua lista está vazia, selecione ingredientes para iniciar!
-      </p>
-    </section>
-
+    <SuaLista :ingredientes="ingredientes" />
     <SelecionarIngredientes />
   </main>
 </template>
@@ -58,31 +42,6 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--coral, #f0633c);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #f0633c);
-  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
