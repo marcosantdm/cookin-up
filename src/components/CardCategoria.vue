27980a5d9a0074a38345da9aca2f7a41b1ccdd6f
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-06-23 22:16:51 -0300

    feat: alterando estado do ingrediente com o click

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -2,13 +2,14 @@
 import type ICategoria from "@/interfaces/iCategoria";
 import type { PropType } from "vue";
 import Tag from "./Tag.vue";
+import IngredienteSelecionavel from "./IngredienteSelecionavel.vue";
 
 export default {
   props: {
     categoria: { type: Object as PropType<ICategoria>, required: true },
   },
 
-  components: { Tag },
+  components: { Tag, IngredienteSelecionavel},
 };
 </script>
 
@@ -26,7 +27,7 @@ export default {
 
     <ul class="categoria__ingredientes">
       <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-        <Tag :texto="ingrediente" />
+        <IngredienteSelecionavel :ingrediente="ingrediente" />
       </li>
     </ul>
   </article>
